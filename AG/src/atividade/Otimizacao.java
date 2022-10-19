1d6d0c1e2c430ed3d62457e5300944fa49ed1dfd
Alysson <54059977+AlyssonGmes@users.noreply.github.com>
2022-10-19 17:14:25 -0300

    Update Otimizacao.java

--- AG/src/atividade/Otimizacao.java
+++ AG/src/atividade/Otimizacao.java
@@ -1,17 +1,12 @@
 package atividade;
 
 public class Otimizacao {
-    double valorA = 0;
-    double valorB = 0;
-    double resultado = 0;
-
+    double valorA = 0, valorB = 0, resultado = 0;
     public Otimizacao(double a, double b) {
         valorA = a;
         valorB = b;
-
         resultado = funcaoOtimiza((int) a, (int)b);
     }
-
     public static double funcaoOtimiza(double a, double b) {
         return 2 * (Math.pow(a, 2)) + b - 57;
     }
